dabab6b12e9aa0446fa919db05bccdf60435ea41
unknown <svyat.teterin@gmail.com>
2021-04-11 17:48:00 +0300

    feat:

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function(event) { 
+/*document.addEventListener("DOMContentLoaded", function(event) { 
     const modal = document.querySelector('.modal');
     const modalBtn = document.querySelectorAll('[data-toggle=modal]');
     const closeBtn = document.querySelector('.modal__close');
@@ -10,4 +10,59 @@ document.addEventListener("DOMContentLoaded", function(event) {
     });
 
     closeBtn.addEventListener('click', switchModal);
-  });
+    window.onclick =function(event) {
+        if (event.target == modal) {
+            modal.classList.toggle('modal--visible');
+        }
+    }
+    // Нерабочая клавиша Esc
+     document.addEventListener ('keypress', function (e) {
+        if (e.code == 'Backquote') {
+            modal.classList.toggle('modal--visible');
+        }
+      }); 
+}); */
+
+
+
+$(document).ready(function () {
+    var modal = $('.modal'), 
+        modalBtn = $('[data-toggle=modal]'),
+        closeBtn = $('.modal__close');
+
+    modalBtn.on('click', function () {
+        modal.toggleClass('modal--visible');
+    });
+    closeBtn.on('click', function () {
+        modal.toggleClass('modal--visible');
+    });
+    var next = $('.swiper-button-next');
+    var prev = $('.swiper-button-prev');
+    var bullets = $('.swiper-pagination');
+    next.css('left', prev.width() + 10 + bullets.width() +10);
+    bullets.css('left', prev.width() + 10);
+});
+
+document.addEventListener("DOMContentLoaded", function(event) { 
+    const swiper = new Swiper('.swiper-container', {
+        // Optional parameters
+        loop: true,
+      
+        // If we need pagination
+        pagination: {
+          el: '.swiper-pagination',
+          type: 'bullets',
+        },
+      
+        // Navigation arrows
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+      
+        // And if we need scrollbar
+        scrollbar: {
+          el: '.swiper-scrollbar',
+        },
+      });
+});
